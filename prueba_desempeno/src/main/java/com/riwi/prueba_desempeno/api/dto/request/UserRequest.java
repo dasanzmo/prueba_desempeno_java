8b2b50eab25b9541b257fdd3accbe2d9834ecc47
danielsm <danielsanchezm92@gmail.com>
2024-06-04 07:20:52 -0500

    add entities and hexagonal structure

---
+++ prueba_desempeno/src/main/java/com/riwi/prueba_desempeno/api/dto/request/UserRequest.java
@@ -0,0 +1,5 @@
+package com.riwi.prueba_desempeno.api.dto.request;
+
+public class UserRequest {
+    
+}
---
+++ prueba_desempeno/src/main/java/com/riwi/prueba_desempeno/domain/repositories/SurveyRepository.java
@@ -0,0 +1,13 @@
+package com.riwi.prueba_desempeno.domain.repositories;
+
+//import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.riwi.prueba_desempeno.domain.entities.Survey;
+
+@Repository
+public interface  SurveyRepository extends JpaRepository<Survey, Long>{
+    //public List<Survey> findByUserId(String id);
+}
